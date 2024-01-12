095effe5535c78fcc209963e7840187e9d58a80f
Mikael88 <mikaelnusantara@gmail.com>
2024-01-12 14:17:08 +0700

    adding constan for /book/{bookId}

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -6,9 +6,10 @@ import (
 )
 
 var RegisterBookStoreRoutes = func(router *mux.Router) {
+	const rute string = "/book/{bookId}" 
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controllers.GetBooks).Methods("GET")
-	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
-	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
-	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+	router.HandleFunc(rute, controllers.GetBookById).Methods("GET")
+	router.HandleFunc(rute, controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc(rute, controllers.DeleteBook).Methods("DELETE")
 }
